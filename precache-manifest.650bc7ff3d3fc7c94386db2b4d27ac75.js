6d0dfdf1011df27e0388313abab5fb585aa10596
Sarot Busala <sabuza97@gmail.com>
2019-04-18 20:38:59 +0700

    Updates

---
+++ precache-manifest.650bc7ff3d3fc7c94386db2b4d27ac75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "e90c00ba807e96754e96",
+    "url": "/home/static/js/main.e505a8fe.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/home/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e90c00ba807e96754e96",
+    "url": "/home/static/css/main.51010422.chunk.css"
+  },
+  {
+    "revision": "5a196be4e70ec66f02c4e96c4876ca12",
+    "url": "/home/index.html"
+  }
+];
