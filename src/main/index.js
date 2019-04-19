8c438f505da47ed63a6f6d47fd1e9b3973d3c8f6
Sarot Busala <sabuza97@gmail.com>
2019-04-19 23:25:42 +0700

    Fix bg

--- src/main/index.js
+++ src/main/index.js
@@ -8,14 +8,14 @@ import './styles.css';
 class MainIndex extends Component {
   render() {
     return (
-      <div class = "bg-css">
-        <div class = "container">
-        <div class = "bg-content">
-          <HeaderComponent />
-          <div class = "home-content">
-            <HomeComponent />
+      <div class="bg-css">
+        <div class="container">
+          <div class="bg-content">
+            <HeaderComponent />
+            <div class="home-content">
+              <HomeComponent />
+            </div>
           </div>
-        </div>
         </div>
       </div>
     );
