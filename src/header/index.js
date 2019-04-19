b3d3efecfb094df8f7204cf7f73ba294bfdf2094
Sarot Busala <sabuza97@gmail.com>
2019-04-19 09:05:58 +0700

    Fix header

--- src/header/index.js
+++ src/header/index.js
@@ -9,7 +9,10 @@ class HeaderComponent extends Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">Home<span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Link</a>
             </li>
             <li className="nav-item active">
               <a className="nav-link" href="/">Link</a>
