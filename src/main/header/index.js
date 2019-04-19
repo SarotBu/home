9e47d7285f6826a0346e028b3b7d4b931885aa03
Sarot Busala <sabuza97@gmail.com>
2019-04-19 21:08:59 +0700

    edit css

--- src/main/header/index.js
+++ src/main/header/index.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 
 class HeaderComponent extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="/home">Sarot Busala</a>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Projects<span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/home">Projects<span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="/">Publications</a>
+              <a className="nav-link" href="/home">Publications</a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="/">Blogs</a>
+              <a className="nav-link" href="/home">Blogs</a>
             </li>
-            
+
           </ul>
-        </div>
       </nav>
-                );
-            }
-        }
-        
-        export default HeaderComponent;
+    );
+  }
+}
+
+export default HeaderComponent;
