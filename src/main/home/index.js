a28758d26a2b5bf873623efb2bdf6de2b23c70d6
Sarot Busala <sarot.busala@gmail.com>
2020-02-01 05:03:38 +0700

    Update index

--- src/main/home/index.js
+++ src/main/home/index.js
@@ -16,8 +16,19 @@ class HomeComponent extends Component {
         <div className="col-8 home-item">
           <p>
             <br/>
-            Hi ! I'm <b>Sarot Busala</b>, a mere mortal trying to be a researcher in Computer Graphics. <br/>
-            Thank you for visiting my site !<br/>
+            Hi ! I'm <b>Sarot Busala</b>. <br/>
+            I'm aspiring to become a researcher in Computer Graphics. <br/>
+            My ultimate goal is to piece through the very limit and boundary of game industry <br/>
+            by developing and introducing a way humans create digital contents <br/>
+            My main focus is in Inverse Rendering, but I also have interest in fields <br/>
+            such as Realtime Physics Simulations, Computer Animation. <br/>
+            I strongly believe digital contents in the future would be <br/>
+            the thing that is inherent in humans' development and also <br/>
+            a main part of how our society would become !<br/>
+            <br/>
+            I'm currently a 1st year master student at Chulalongkorn University. <br/>
+            My plan is to continue my study on PhD abroad ! (The location has not yet decided). <br/>
+            Thank you for visiting my personal site !<br/>
             <br/>
             初めまして！私は　「サロト　ブサラー」　(Sarot Busala)　と申します！<br/>
             私の目標はコンピュターグラフィックスの研究者になることです！ <br/>
