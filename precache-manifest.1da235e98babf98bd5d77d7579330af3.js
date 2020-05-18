002e118006c348dff4ae25d160a2623b0c9237f6
Sarot <sabuza97@gmail.com>
2020-05-18 16:45:12 +0700

    Updates

---
+++ precache-manifest.1da235e98babf98bd5d77d7579330af3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "327364af0ad51dbfcd91",
+    "url": "/home/static/css/main.95393771.chunk.css"
+  },
+  {
+    "revision": "327364af0ad51dbfcd91",
+    "url": "/home/static/js/main.7d9b52cb.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "49fe2bb6337391b45054",
+    "url": "/home/static/js/2.6e39e941.chunk.js"
+  },
+  {
+    "revision": "160cf9df0d03b35d81beb040e6780885",
+    "url": "/home/static/media/profile.160cf9df.png"
+  },
+  {
+    "revision": "98b04da4f27efde5cc7bd96bbf0c0e18",
+    "url": "/home/static/media/Sarot_Resume.98b04da4.pdf"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/home/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d3c07b9611ae2d09e0f2d28d8194033e",
+    "url": "/home/static/media/default_bg.d3c07b96.jpg"
+  },
+  {
+    "revision": "05e7c860a9015996978218a8f4de26ce",
+    "url": "/home/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.44171a90800df2f2061cc625f2979cda.js"
+  "/home/precache-manifest.1da235e98babf98bd5d77d7579330af3.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/home/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
