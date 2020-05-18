c0d7c942115882598b8b45a9b805d26bc80a3064
Sarot <sabuza97@gmail.com>
2020-05-18 17:08:23 +0700

    Update Resume

--- src/main/home/index.js
+++ src/main/home/index.js
@@ -20,8 +20,13 @@ class HomeComponent extends Component {
             I'm aspiring to become a researcher in Computer Graphics. <br/>
             My ultimate goal is to piece through the very limit and boundary of game industry <br/>
             by developing and introducing a way humans create digital contents <br/>
-            My main focus is in Inverse Rendering, but I also have interest in fields <br/>
-            such as Realtime Physics Simulations, Computer Animation. <br/>
+            My main focus is in researching better way to create 3D model with less effort. <br/>
+	    The way we create 3D model now is quite linear and we could have done much better ! <br/>
+ 	    My research focus is in applying multi-view, photometric stereo and deep learning together to achieve the aforementioned goal. <br/>
+	    My interest is actually in anything that could make game development becomes less costly and more enjoyable ! <br/>
+	    I focus on 3D modeling, but another aspects are all important too. Whether it is in coding, modeling, animating, etc.<br/>
+	    There are countless things that could be improved. <br/>
+	    Many people around the world contributing are in their own way. I hope my work could somehow help ! :) <br/>
             I strongly believe digital contents in the future would be <br/>
             the thing that is inherent in humans' development and also <br/>
             a main part of how our society would become !<br/>
